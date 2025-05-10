7b45808e51d3aeaf024d555d4c80bdf36bb8ace1
Connor <107011324+connor33341@users.noreply.github.com>
2025-05-09 21:34:58 -0400

    update

---
+++ test/create-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# URL of the shell script to download
+SCRIPT_URL="https://raw.githubusercontent.com/connor33341/pypack/refs/heads/main/scripts/wrappers/linux.sh"
+
+# Name of the downloaded file
+SCRIPT_NAME="pypack.sh"
+
+# Download the shell script
+curl -o "$SCRIPT_NAME" "$SCRIPT_URL"
+
+# Make the downloaded script executable
+chmod +x "$SCRIPT_NAME"
+
+# Execute the downloaded script
+./"$SCRIPT_NAME"
